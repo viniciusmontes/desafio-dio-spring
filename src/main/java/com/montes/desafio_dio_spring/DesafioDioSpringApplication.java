26becf227d1aa08bd8475d0ee8ee863ebb5d6c45
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-07-15 14:29:04 -0300

    Project created

---
+++ src/main/java/com/montes/desafio_dio_spring/DesafioDioSpringApplication.java
@@ -0,0 +1,13 @@
+package com.montes.desafio_dio_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDioSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montes/desafio_dio_spring/DesafioDioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montes.desafio_dio_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
